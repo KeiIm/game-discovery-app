6b608a07fe9cc6541f2a36f41c765f7f9c046a8b
Kei <marios_bestfriend@htmail.com>
2024-01-12 15:07:27 -0500

    Provide placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import placeholder from '../assets/no-image-placeholder.webp'
+
 const getCroppedImageUrl = (url: string) => {
     // Temporary fix to deal with games without images
-    if (!url) return '';
+    if (!url) return placeholder;
     
     const index = url.indexOf('media/') + 'media/'.length
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
