984b06b749fd4e60c4bf9710b64158d9ddf087bb
Kei <marios_bestfriend@htmail.com>
2024-01-12 12:20:24 -0500

    Sort games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,7 @@
 const getCroppedImageUrl = (url: string) => {
+    // Temporary fix to deal with games without images
+    if (!url) return '';
+    
     const index = url.indexOf('media/') + 'media/'.length
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
