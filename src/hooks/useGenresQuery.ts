53d1489634020908fbe28169d134f6a24aed1e90
Kei <marios_bestfriend@htmail.com>
2024-01-29 14:44:33 -0500

    Implement ReactQuery, Refactor useGenres

--- src/hooks/useGenresQuery.ts
+++ src/hooks/useGenresQuery.ts
@@ -1,21 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
-import genres from "../data/genres";
 
 export interface Genre {
     id: number;
     name: string;
     image_background: string;
   }
-  
-const useGenres = () => useQuery({
+
+const useGenresQuery = () => useQuery<Genre[], Error>({
   queryKey: ['genres'],
   queryFn: () => apiClient
     .get<FetchResponse<Genre>>('/genres')
-    .then(res => res.data),
-    staleTime: 24 * 60 * 60_000, // 24h
-    initialData: { count: genres.length, results: genres}
+    .then((res)=>res.data),
+    staleTime: 60_000
 })
 
-export default useGenres
+export default useGenresQuery
