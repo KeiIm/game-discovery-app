db196fd0b6e936680400e3fa159b03e5ff6c044a
Kei <marios_bestfriend@htmail.com>
2024-01-31 17:37:11 -0500

    Implement useQuery with Platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -20,7 +20,7 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
       </MenuButton>
 
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
