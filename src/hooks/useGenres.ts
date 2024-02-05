e2779cc57fc713a2365095ce17e5b3ecfa95dd03
Kei <marios_bestfriend@htmail.com>
2024-02-05 13:37:39 -0500

    Refactor - simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from 'ms';
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
@@ -13,7 +14,8 @@ export interface Genre {
 const useGenres = () => useQuery({
   queryKey: ['genres'],
   queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60_000, // 24h
+    // staleTime: 24 * 60 * 60_000, // 24h
+    staleTime: ms('1 day'),
     initialData: genres
 })
 
