4f8685954d625146d6bcee73b784293f2462c529
Kei <marios_bestfriend@htmail.com>
2024-02-09 15:53:52 -0500

    Setup routing

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,11 @@
+import { GridItem } from '@chakra-ui/react';
+
+const GameDetailPage = () => {
+  return (
+    <>
+      <GridItem area="main">GameDetails</GridItem>
+    </>
+  );
+};
+
+export default GameDetailPage;
