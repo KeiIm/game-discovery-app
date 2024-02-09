1ceaf3d4f0fee2dad02a165dfcfd023db918f5a6
Kei <marios_bestfriend@htmail.com>
2024-02-09 17:29:15 -0500

    Fetch game details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,19 @@
-import { GridItem } from '@chakra-ui/react';
+import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
   return (
     <>
-      <GridItem area="main">GameDetails</GridItem>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
     </>
   );
 };
