177b1d5188c5590b5dbf30dad9feb941f8254e68
Kei <marios_bestfriend@htmail.com>
2024-02-12 17:29:59 -0500

    Improve layout and navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <HStack padding="10px">
-      <Image boxSize="60px" src={logo} />
+      <Link to="/">
+        <Image boxSize="60px" src={logo} objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
